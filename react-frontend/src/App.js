dd9f6b59a425e550ddc70f4cef6645c85eaca24a
Thiago Carvalho <carvalho@pythian.com>
2019-03-10 21:58:21 -0300

    small updates in app.js to add toastfy and poll route path

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,9 @@
+// community modules
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
+import { ToastContainer } from "react-toastify";
+
+// local modules
 import NavBar from "./components/navBar";
 import Home from "./components/home";
 import Features from "./components/features";
@@ -8,23 +11,43 @@ import Architecture from "./components/architecture";
 import MainPoll from "./components/mainPoll";
 import About from "./components/about";
 import NotFound from "./components/notFound";
+import RegisterForm from "./components/registerForm";
+import LoginForm from "./components/loginForm";
+import Logout from "./components/logout";
+import auth from "./services/authService";
+//import ProtectedRoute from "./components/common/protectedRoute";
+
+// CSS
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <React.Fragment>
-        <NavBar />
+        <ToastContainer />
+        <NavBar user={user} />
         <main className="container">
           <Switch>
             <Route path="/home" component={Home} />
             <Route path="/features" component={Features} />
             <Route path="/architecture" component={Architecture} />
-            <Route path="/poll" component={MainPoll} />
+            <Route path="/poll" render={() => <MainPoll user={user} />} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/about" component={About} />
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/home" />
             <Redirect to="/not-found" />
-            <Redirect to="/home" />
           </Switch>
         </main>
       </React.Fragment>
